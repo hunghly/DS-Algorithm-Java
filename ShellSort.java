c872843ebc7331a84b79285600be7df7bdbc14dc
hunghly <hunghly@gmail.com>
2020-06-23 10:00:04 -0500

    finished shellsort

--- ShellSort.java
+++ ShellSort.java
@@ -20,9 +20,17 @@ public class ShellSort {
 
         For this example, we will use array.length / 2 which is 3
         */
-        int gap = intArray.length / 2;
-        System.out.println(gap);
-        // int (int i = 0; )
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+                int j = i;
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j-gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
