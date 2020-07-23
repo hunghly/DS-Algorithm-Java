fad2eb5aa7a28f869eb9c1e42b29c270660cbc28
Hung Ly <hunghly@gmail.com>
2020-07-22 20:28:07 -0500

    finished ArrayList test

---
+++ lists/arraylists/ArrayListTest.java
@@ -0,0 +1,14 @@
+package lists.arraylists;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        newArrayList.add(1);
+        newArrayList.add(2);
+        newArrayList.add(3);
+        System.out.println(newArrayList.get(0));
+
+    }
+}
---
+++ lists/arraylists/Main.java
@@ -0,0 +1,44 @@
+package lists.arraylists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ArrayList is good for random access and if you know the index. Good for iterating over the list but not for inserting/deletion/removing and accessing if you do not know the index.
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Hung", "Ly", 123));
+        employeeList.add(new Employee("Mike", "Jordan", 2323));
+        employeeList.add(new Employee("David", "Blaine", 100));
+        employeeList.add(new Employee("Larry", "Bird", 33));
+
+        // printing employees
+//        employeeList.forEach(employee -> System.out.println(employee));
+//        System.out.println(employeeList.get(1)); // O(1)
+//        System.out.println(employeeList.isEmpty());
+
+        // replacing employee
+        employeeList.set(2, new Employee("Magic", "Johnson", 32)); // O(1)
+//        employeeList.forEach(employee -> System.out.println(employee));
+        employeeList.add(3, new Employee("Steve", "Young", 11));
+//        employeeList.forEach(employee -> System.out.println(employee));
+
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+
+        for (Employee employee : employeeArray) {
+            System.out.println(employee);
+        }
+
+        // will print false because this
+        System.out.println(employeeList.contains(new Employee("Steve", "Young", 11)));
+
+        System.out.println(employeeList.indexOf(new Employee("Magic", "Johnson", 32)));
+
+        employeeList.remove(2);
+
+        employeeList.forEach(employee -> System.out.println(employee));
+    }
+}
