bad76577a2c8a10298fb8d7054e026ae0bedff07
Hung Ly <hunghly@gmail.com>
2020-07-22 22:17:06 -0500

    finished linked list implementation

--- lists/linkedlists/EmployeeLinkedList.java
+++ lists/linkedlists/EmployeeLinkedList.java
@@ -5,11 +5,36 @@ import lists.arraylists.Employee;
 public class EmployeeLinkedList {
 
     private EmployeeNode head;
+    private int size = 0;
 
     public void addToFront(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
+        // sets the new node's next value to the current list's current head (which is null);
+
         node.setNext(head);
         this.head = node;
+        this.size++;
+    }
+
+    public EmployeeNode removeFromFront() {
+        if (isEmpty()) {
+            return null;
+        }
+        EmployeeNode removedNode = this.head;
+        this.head = this.head.getNext();
+        // decrease the size after remove
+        size--;
+        // clean up the references
+        removedNode.setNext(null);
+        return removedNode;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
     }
 
     public void printList() {
--- lists/linkedlists/Main.java
+++ lists/linkedlists/Main.java
@@ -5,6 +5,8 @@ import lists.arraylists.Employee;
 public class Main {
 
     public static void main(String[] args) {
+        // disadvantage of linked list is storing the extra value for memory.
+        // LL is a good choice if you want what's in front of the list; if you want random data it might not be a good choice.
         Employee hungLy = new Employee("Hung", "Ly", 123);
         Employee michaelJordan = new Employee("Michael", "Jordan", 23);
         Employee magicJohnson = new Employee("Magic", "Johnson", 32);
@@ -16,6 +18,11 @@ public class Main {
         list.addToFront(magicJohnson);
         list.addToFront(larryBird);
 
+        System.out.println(list.getSize());
+        System.out.println(list.isEmpty());
+        list.printList();
+        EmployeeNode removedNode = list.removeFromFront();
+        System.out.println("removed " + removedNode);
         list.printList();
     }
 
